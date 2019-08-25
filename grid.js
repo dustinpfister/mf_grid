fc1b4997a94923c642b6d011114326f695e897c7
dustin pfister <dustin.pfister@gmail.com>
2019-08-25 19:32:02 -0400

    get neighbors

--- grid.js
+++ grid.js
@@ -1,3 +1,4 @@
+// Node Constructor
 var Node = function(opt){
     var opt = opt || {};
     this.x = opt.x;
@@ -6,15 +7,13 @@ var Node = function(opt){
     this.walkable = opt.walkable === undefined ? true: opt.walkable;
 };
 
+// Main Grid Constructor
 var Grid = function(opt){
-    
     opt = opt || {};
     this.w = opt.w || 8;
     this.h = opt.h || 6;
     this.nodes = [];
-    
     this.buildCleanNodes();
-    
 };
 
 // just build a clean array of nodes for the gird
@@ -42,4 +41,27 @@ Grid.prototype.buildCleanNodes = function(){
 // return truw if the given x and y position is in bounds
 Grid.prototype.isInBounds = function(x,y){
     return (x >= 0 && x < this.w) && (y >= 0 && y < this.h);
+};
+
+// get the four Neighbors of a node
+Grid.prototype.getNeighbors = function(node){
+    var x = node.x,
+    y = node.y,
+    neighbors = [];
+
+    if(this.isInBounds(x,y-1)){
+        neighbors.push(this.nodes[y-1][x]);
+    }
+    if(this.isInBounds(x,y + 1)){
+        neighbors.push(this.nodes[y+1][x]);
+    }
+    if(this.isInBounds(x - 1,y)){
+        neighbors.push(this.nodes[y][x-1]);
+    }
+    if(this.isInBounds(x + 1,y)){
+        neighbors.push(this.nodes[y][x+1]);
+    }
+    
+    return neighbors;    
+    
 };
